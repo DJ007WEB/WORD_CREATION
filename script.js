9dc3bf4218630fb004a9ae3c817a89ebc5797fa4
DJ007WEB <007officialjyo@gmail.com>
2023-09-06 21:18:41 +0530

    added page 2

--- script.js
+++ script.js
@@ -20,11 +20,31 @@ mobileNav.addEventListener('click' , () => {
 const slider = document.querySelectorAll('.p2-main img');
 
 
-console.log(slider);
+// console.log(slider);
 
 slider.forEach((img, index) => {
     img.style.left = `${index * 100}%`;
 })
 
 let counter = 0;
+
+const leftBtn = document.querySelector('.left-btn');
+
+console.log(leftBtn);
+
+leftBtn.addEventListener('click', () => {
+    counter--;
+    sliderWork(counter);
+})
+
+document.querySelector('.right-btn').addEventListener('click', () => {
+   counter++;
+   sliderWork(counter);
+})
+
+function sliderWork(i) {
+    slider.forEach((img) => {
+        img.style.transform = `translateX(-${i * 100}%)`;
+    })
+}
 
